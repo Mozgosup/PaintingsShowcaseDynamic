eb4120b53cbadba735e31257b9f2289c0da0e8e0
Mozgosup <30355965+Mozgosup@users.noreply.github.com>
2025-06-05 22:38:06 +0200

    Remove debug image from Artwork component

--- frontend/src/components/Artwork.js
+++ frontend/src/components/Artwork.js
@@ -23,14 +23,6 @@ function Artwork({ painting }) {
                 <p>Year: {painting.year}</p>
                 {/* ...и т.д. если хотите показать height/width */}
             </figcaption>
-            <div style={{marginTop: '10px', border: '2px solid green'}}>
-                <p style={{color: 'red'}}>Debug test image:</p>
-                <img
-                    src="https://paintingsbucket-mozgosup.s3.eu-north-1.amazonaws.com/paintings/7f97addb-c276-47ba-9f13-7429a08dcc9a-Roof_Tyutchevo.jpeg"
-                    alt="debug test"
-                    style={{border: '2px dashed red', display: 'block'}}
-                />
-            </div>
         </figure>
     );
 }
